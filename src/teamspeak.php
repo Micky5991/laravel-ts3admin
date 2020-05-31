d56a6a3c71cffebddca21f357ceefa3e848a6220
Francesco Paolocci <micky5991@gmail.com>
2020-05-31 10:18:55 +0200

    Add comments to teamspeak.php to explain the settings

--- src/teamspeak.php
+++ src/teamspeak.php
@@ -2,22 +2,49 @@
 
 return [
 
+    /*
+     * Determines if the connection should be established automatically
+     */
     'connect' => env('TS_CONNECT', true),
 
+    /*
+     * Address of the host machine that hosts the TeamSpeak Server
+     */
     'host' => env('TS_SERVER_HOST', '127.0.0.1'),
 
+    /*
+     * Port of the virtual server instance
+     */
     'port' => env('TS_SERVER_PORT', 9987),
 
+    /*
+     * Maximum time the client is allowed to connect (in seconds)
+     */
     'timeout' => env('TS_SERVER_TIMEOUT', 2),
 
+    /*
+     * Nickname that should be set when the client connected.
+     */
     'nickname' => 'Laravel Webinterface',
 
+    /*
+     * Query specific credentials
+     */
     'query' => [
 
+        /*
+         * Port of the query interface the the TeamSpeak server
+         */
         'port' => env('TS_QUERY_PORT', 10011),
 
+        /*
+         * Username that will be used to authenticate with the server query.
+         */
         'username' => env('TS_QUERY_USER', 'serveradmin'),
 
+        /*
+         * Password that will be used to authenticate with the server query.
+         */
         'password' => env('TS_QUERY_PASSWORD')
 
     ]
